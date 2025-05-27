4742dfa5d966671a818ce097d4e8ab355623ad68
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-05-27 17:13:14 +0530

    added header

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import { PenBox } from 'lucide-react'
+
+function Header() {
+    return (
+        <nav className='mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2'>
+            <Link href={"/"} className='flex items-center'>
+                <Image src='/logo.png'
+                    height="60"
+                    width="150"
+                    alt='Schedular Logo'
+                    className='h-16 w-auto'
+                />
+            </Link>
+            <div className='flex items-center gap-4'>
+                <Link href='/events?create=true'>
+                    <Button className='flex items-center gap-2 '>
+                        <PenBox size={18} />
+                        Create Event
+                    </Button>
+                </Link>
+                <Button variant='outline'>
+                    Login
+                </Button>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
