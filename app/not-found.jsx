09f7b2a9b4e04f260b7aaf6d072d780ae4a2b061
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-06-02 22:48:51 +0530

    added NotFound Page

---
+++ app/not-found.jsx
@@ -0,0 +1,13 @@
+
+const NotFound = () => {
+    return (
+        <div className="text-4xl font-extrabold w-screen pt-96 grid justify-center">
+            <h1 className="">
+                404 - Page Not Found
+            </h1>
+
+        </div>
+    )
+}
+
+export default NotFound
